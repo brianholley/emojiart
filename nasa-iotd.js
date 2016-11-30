090468a1b649379d91559df82a840dabc101f64d
Brian Holley <brian.holley@hotmail.com>
2016-11-29 20:30:06 -0800

    Convert NASA APOD to promises

--- nasa-iotd.js
+++ nasa-iotd.js
@@ -1,15 +1,21 @@
 var HttpUtils = require('./httputils')
 
-function imageOfTheDay(apiKey, callback) {
-    let url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey
-    HttpUtils.getJson(
-        url, 
-        (image) => {
-            let imageUrl = image.url
-            let altText = image.title
-            HttpUtils.getBinary(imageUrl, (data) => {
-                callback(imageUrl, data, altText)
-            })
+function imageOfTheDay(apiKey) {
+    return new Promise((resolve, reject) => {
+        let url = 'https://api.nasa.gov/planetary/apod?api_key=' + apiKey
+        HttpUtils.getJson(
+            url, 
+            (image) => {
+                let imageUrl = image.url
+                let altText = image.title
+                HttpUtils.getBinary(imageUrl, (data) => {
+                    resolve({
+                        imageUrl: imageUrl, 
+                        imageData: data, 
+                        altText: altText
+                    })
+                })
+        })
     })
 }
 
