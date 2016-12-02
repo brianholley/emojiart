9f901087c0818e01072062cd6adfbd5c89188ecc
Brian Holley <brian.holley@hotmail.com>
2016-12-01 23:03:03 -0800

    Restructure twitter bot apis, make test in index.js explicit

--- index.js
+++ index.js
@@ -1,43 +1,98 @@
 var fs = require('fs')
+var os = require('os')
+var path = require('path')
 require('use-strict')
 
+var Bing = require('./bing-iotd')
 var mosaic = require('./mosaic.js')
+var Nasa = require('./nasa-iotd')
+var TwitterReplyBot = require('./twitter-replybot')
 
-function main(input, output) {
-    const emojiSize = 16
-
-    var colorTableFile = './cache/colors.json' 
-    if (!fs.existsSync(colorTableFile)) {
-        console.log("Generating color table")
-        var colors = mosaic.createColorTable('./emojis/e1-png/png_512', './cache', emojiSize)
-        fs.writeFileSync(colorTableFile, JSON.stringify(colors))
-    }
-
-    var colorTable = JSON.parse(fs.readFileSync(colorTableFile))
-    mosaic.generate(fs.readFileSync(input), output, colorTable, {emojiSize: emojiSize})
-        .then(() => {})
-        .catch((reason) => { console.log(reason)})
+const emojiSize = 16
+// TODO: Save to os.homedir()
+// TODO: Refactor into mosaic
+var colorTableFile = './cache/colors.json' 
+if (!fs.existsSync(colorTableFile)) {
+    console.log("Generating color table")
+    var colors = mosaic.createColorTable('./emojis/e1-png/png_512', './cache', emojiSize)
+    fs.writeFileSync(colorTableFile, JSON.stringify(colors))
 }
 
-if (process.argv.length < 4) {
-    console.log("Usage: InputImage OutputImage")
+var colorTable = JSON.parse(fs.readFileSync(colorTableFile))
+
+// Test usage: test <input> <output>
+if (process.argv.length >= 5 && process.argv[2] == "test") {
+    let input = process.argv[3]
+    let output = process.argv[4]
+    console.log(`TEST MODE: ${input} => ${output}`)
+
+    if (input == "-bing") {
+        Bing.imageOfTheDay().then((image) => {
+            let iotd = path.join(os.tmpdir(), 'bingiotd.jpg')
+            fs.writeFileSync(iotd, image.imageData, {encoding: 'binary'})
+            return mosaic.generate(fs.readFileSync(iotd), output, colorTable, {emojiSize: emojiSize})
+        })
+        .then(() => { console.log("Finished!") })
+        .catch((reason) => { console.log(reason) })
+
+    } 
+    else if (input == "-nasa") {
+        Nasa.imageOfTheDay(process.env.NASA_API_KEY).then((image) => {
+            let iotd = path.join(os.tmpdir(), 'nasaiotd.jpg')
+            fs.writeFileSync(iotd, image.imageData, {encoding: 'binary'})
+            return mosaic.generate(fs.readFileSync(iotd), output, colorTable, {emojiSize: emojiSize})
+        })
+        .then(() => { console.log("Finished!") })
+        .catch((reason) => { console.log(reason) })
+    } 
+    else {
+        mosaic.generate(fs.readFileSync(input), output, colorTable, {emojiSize: emojiSize})
+            .then(() => { console.log("Finished!") })
+            .catch((reason) => { console.log(reason) })
+    }
     return
 }
 
-main(process.argv[2], process.argv[3])
+function picturesInTweet(tweet) {
+    //tweet.extended_entities.media.expanded_url
+    // [{"id":795115189536366600,"id_str":"795115189536366592","indices":[39,62],"media_url":"http://pbs.twimg.com/media/CwjRDVvVIAAPpqq.jpg","media_url_https":"https://pbs.twimg.com/media/CwjRDVvVIAAPpqq.jpg","url":"https://t.co/hxsXPrWvla","display_url":"pic.twitter.com/hxsXPrWvla","expanded_url":"https://twitter.com/BaileyLAKings/status/795115202345766912/photo/1","type":"photo","sizes":{"large":{"w":2048,"h":1029,"resize":"fit"},"thumb":{"w":150,"h":150,"resize":"crop"},"small":{"w":680,"h":342,"resize":"fit"},"medium":{"w":1200,"h":603,"resize":"fit"}},"source_status_id":795115202345766900,"source_status_id_str":"795115202345766912","source_user_id":205938397,"source_user_id_str":"205938397"}]
 
-// var Bing = require('./bing-iotd')
-// Bing.imageOfTheDay().then(
-//     (image) => {
-//         fs.writeFileSync('./temp/bingiotd.jpg', image.imageData, {encoding: 'binary'})
-//         main('./temp/bingiotd.jpg', './out/bingiotd.png')
-//     })
-// .catch((reason) => {console.log(reason)})
+    var pictures = tweet.entities.media.filter((m) => m.type == "photo").map((m) => {
+        return m.media_url_https
+    })
+    return pictures
+}
 
-// var Nasa = require('./nasa-iotd')
-// Nasa.imageOfTheDay(process.env.NASA_API_KEY).then(
-//     (image) => {
-//         fs.writeFileSync('./temp/nasaiotd.jpg', image.imageData, {encoding: 'binary'})
-//         main('./temp/nasaiotd.jpg', './out/nasaiotd.png')
-//     })
-// .catch((reason) => {console.log(reason)})
+var bot = new TwitterReplyBot({
+    name: "emojiartbot",
+    consumer_key:         process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
+    access_token:         process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    onTweet: (tweet, twit) => {
+        console.log(tweet)
+        console.log('tweet.entities.urls: ' + JSON.stringify(tweet.entities.urls))
+        console.log('tweet.entities.user_mentions: ' + JSON.stringify(tweet.entities.user_mentions))
+        console.log('tweet.extended_entities: ' + JSON.stringify(tweet.extended_entities))
+        
+        if (tweet.extended_entities !== undefined && tweet.extended_entities.media !== undefined) {
+            console.log(JSON.stringify(tweet.extended_entities.media))
+        }
+
+        //let replyTo = '@' + tweet.user.screen_name
+        //let altText = "Emojified art for " + replyTo
+        //postPictureReply(twit, replyTo, tweet.id, altText, './out/circle-tiled.png')
+    }
+})
+bot.start()
+
+var output = './out/bingiotd.png'
+Bing.imageOfTheDay().then((image) => {
+    let iotd = './temp/bingiotd.jpg'
+    fs.writeFileSync(iotd, image.imageData, {encoding: 'binary'})
+
+    return mosaic.generate(fs.readFileSync(iotd), output, colorTable, {emojiSize: emojiSize})
+}).then(() => {
+    bot.tweetReply('Bing image of the day #emojified', 0, output, 'Bing image of the day, emojified')
+})
+.catch((reason) => {console.log(reason)})
--- twitter-replybot.js
+++ twitter-replybot.js
@@ -19,7 +19,9 @@ class TwitterReplyBot {
         })
         this.onTweet = this.onTweet.bind(this)
         this.onTweetAction = params.onTweet
+    }
 
+    start() {
         var stream = this.twit.stream('user') 
         stream.on('tweet', this.onTweet)
     }
@@ -31,81 +33,28 @@ class TwitterReplyBot {
         }
         this.onTweetAction(tweet, this.twit)
     }
-}
 
+    tweetReply(text, replyToId, filepath, altText) {
+        var b64content = fs.readFileSync(filepath, { encoding: 'base64' })
+        this.twit.post('media/upload', { media_data: b64content }, (err, data, response) => {
+            var mediaIdStr = data.media_id_string
+            var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
 
-// T.get('statuses/mentions_timeline', { count: 100 }, function(err, data, response) {
-//   console.log(data)
-// })
-
-function postPictureReply(twit, replyToUser, replyToId, altText, filepath) {
-    var b64content = fs.readFileSync(filepath, { encoding: 'base64' })
-
-    twit.post('media/upload', { media_data: b64content }, (err, data, response) => {
-        var mediaIdStr = data.media_id_string
-        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
-
-        twit.post('media/metadata/create', meta_params, (err, data, response) => {
-            if (err) throw err
-            
-            var params = { 
-                status: replyToUser,
-                in_reply_to_status_id: replyToId, 
-                media_ids: [mediaIdStr] 
-            }
-            twit.post('statuses/update', params, (err, data, response) => {
+            this.twit.post('media/metadata/create', meta_params, (err, data, response) => {
                 if (err) throw err
+                
+                var params = { 
+                    status: text,
+                    in_reply_to_status_id: replyToId, 
+                    media_ids: [mediaIdStr] 
+                }
+                this.twit.post('statuses/update', params, (err, data, response) => {
+                    if (err) throw err
+                    console.log("Tweet success")
+                })
             })
         })
-    })
+    }
 }
 
-var bot = new TwitterReplyBot({
-    name: "emojiartbot",
-    consumer_key:         process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
-    access_token:         process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
-    onTweet: (tweet, twit) => {
-        console.log(tweet)
-        console.log('tweet.entities.urls: ' + JSON.stringify(tweet.entities.urls))
-        console.log('tweet.entities.user_mentions: ' + JSON.stringify(tweet.entities.user_mentions))
-        console.log('tweet.extended_entities: ' + JSON.stringify(tweet.extended_entities))
-        
-        if (tweet.extended_entities !== undefined && tweet.extended_entities.media !== undefined) {
-            console.log(JSON.stringify(tweet.extended_entities.media))
-            //tweet.extended_entities.media.expanded_url
-// [{"id":795115189536366600,"id_str":"795115189536366592","indices":[39,62],"media_url":"http://pbs.twimg.com/media/CwjRDVvVIAAPpqq.jpg","media_url_https":"https://pbs.twimg.com/media/CwjRDVvVIAAPpqq.jpg","url":"https://t.co/hxsXPrWvla","display_url":"pic.twitter.com/hxsXPrWvla","expanded_url":"https://twitter.com/BaileyLAKings/status/795115202345766912/photo/1","type":"photo","sizes":{"large":{"w":2048,"h":1029,"resize":"fit"},"thumb":{"w":150,"h":150,"resize":"crop"},"small":{"w":680,"h":342,"resize":"fit"},"medium":{"w":1200,"h":603,"resize":"fit"}},"source_status_id":795115202345766900,"source_status_id_str":"795115202345766912","source_user_id":205938397,"source_user_id_str":"205938397"}]
-        }
-
-        //let replyTo = '@' + tweet.user.screen_name
-        //let altText = "Emojified art for " + replyTo
-        //postPictureReply(twit, replyTo, tweet.id, altText, './out/circle-tiled.png')
-    }
-})
-
-var fs = require('fs')
-var mosaic = require('./mosaic')
-var Bing = require('./bing-iotd')
-
-var output = './out/bingiotd.png'
-Bing.imageOfTheDay().then((image) => {
-    let iotd = './temp/bingiotd.jpg'
-    fs.writeFileSync(iotd, image.imageData, {encoding: 'binary'})
-
-    const emojiSize = 16
-    var colorTableFile = './cache/colors.json' 
-    if (!fs.existsSync(colorTableFile)) {
-        console.log("Generating color table")
-        var colors = mosaic.createColorTable('./emojis/e1-png/png_512', './cache', emojiSize)
-        fs.writeFileSync(colorTableFile, JSON.stringify(colors))
-    }
-
-    var colorTable = JSON.parse(fs.readFileSync(colorTableFile))
-    return mosaic.generate(fs.readFileSync(iotd), output, colorTable, {emojiSize: emojiSize})
-}).then(() => {
-    postPictureReply(bot.twit, 'Bing image of the day #emojified', 0, 'Bing image of the day, emojified', output)
-})
-.catch((reason) => {console.log(reason)})
-
-    
+module.exports = TwitterReplyBot
