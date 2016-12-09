0bb5beb02d4f7ebf2692b26dd11fb8f2063399b5
Brian Holley <brian.holley@hotmail.com>
2016-12-08 21:48:59 -0800

    Actually get im working

--- index.js
+++ index.js
@@ -2,25 +2,18 @@ var fs = require('fs')
 var schedule = require('node-schedule')
 var os = require('os')
 var path = require('path')
-var rp = require('request-promise-native')
 require('use-strict')
 
 var Bing = require('./bing-iotd')
-var mosaic = require('./mosaic.js')
+var mosaic = require('./mosaic')
 var Nasa = require('./nasa-iotd')
 var TwitterReplyBot = require('./twitter-replybot')
 
 const emojiSize = 16
-// TODO: Save to os.homedir()
-// TODO: Refactor into mosaic
-var colorTableFile = './cache/colors.json' 
-if (!fs.existsSync(colorTableFile)) {
-    console.log("Generating color table")
-    var colors = mosaic.createColorTable('./emojis/e1-png/png_512', './cache', emojiSize)
-    fs.writeFileSync(colorTableFile, JSON.stringify(colors))
-}
-
-var colorTable = JSON.parse(fs.readFileSync(colorTableFile))
+var tileset = new mosaic.Tileset(
+    path.join('.', 'emojis', 'e1-png', 'png_512'),
+    path.join(os.homedir(), 'emojis'),
+    emojiSize)
 
 // Test usage: test <input> <output>
 if (process.argv.length >= 5 && process.argv[2] == "test") {
@@ -32,7 +25,7 @@ if (process.argv.length >= 5 && process.argv[2] == "test") {
         Bing.imageOfTheDay().then((image) => {
             let iotd = path.join(os.tmpdir(), 'bingiotd.jpg')
             fs.writeFileSync(iotd, image.imageData, {encoding: 'binary'})
-            return mosaic.generate(iotd, output, colorTable, {emojiSize: emojiSize})
+            return mosaic.generate(iotd, output, tileset, {emojiSize: emojiSize})
         })
         .then(() => { console.log("Finished!") })
         .catch((reason) => { console.log(reason) })
@@ -42,13 +35,13 @@ if (process.argv.length >= 5 && process.argv[2] == "test") {
         Nasa.imageOfTheDay(process.env.NASA_API_KEY).then((image) => {
             let iotd = path.join(os.tmpdir(), 'nasaiotd.jpg')
             fs.writeFileSync(iotd, image.imageData, {encoding: 'binary'})
-            return mosaic.generate(iotd, output, colorTable, {emojiSize: emojiSize})
+            return mosaic.generate(iotd, output, tileset, {emojiSize: emojiSize})
         })
         .then(() => { console.log("Finished!") })
         .catch((reason) => { console.log(reason) })
     } 
     else {
-        mosaic.generate(input, output, colorTable, {emojiSize: emojiSize})
+        mosaic.generate(input, output, tileset, {emojiSize: emojiSize})
             .then(() => { console.log("Finished!") })
             .catch((reason) => { console.log(reason) })
     }
@@ -78,7 +71,7 @@ var bot = new TwitterReplyBot({
                 fs.writeFileSync(inputFile, data, {encoding: 'binary'})
                 
                 let outputFile = path.join(os.tmpdir(), `tweet_${tweet.id}_emoji.png`)
-                mosaic.generate(inputFile, outputFile, colorTable, {
+                mosaic.generate(inputFile, outputFile, tileset, {
                     emojiSize: emojiSize
                 })
                 .then(() => {
@@ -132,7 +125,7 @@ var j = schedule.scheduleJob(rule, () => {
         url = image.imageUrl
         let iotd = path.join(os.tmpdir(), 'source.jpg')
         fs.writeFileSync(iotd, image.imageData, {encoding: 'binary'})
-        return mosaic.generate(fs.readFileSync(iotd), outputFile, colorTable, {emojiSize: emojiSize})
+        return mosaic.generate(fs.readFileSync(iotd), outputFile, tileset, {emojiSize: emojiSize})
     })
     .then(() => { 
         console.log(`Emojification complete`)
@@ -160,20 +153,4 @@ var j = schedule.scheduleJob(rule, () => {
 //             media_url_https: "http://mtv.mtvnimages.com/uri/mgid:file:http:shared:public.articles.mtv.com/wp-content/uploads/2014/08/2001-a-space-odyssey.jpg?width=480&quality=0.85&maxdimension=2000"
 //         }]
 //     }
-// })
-
-// var output = './out/bingiotd.png'
-// Bing.imageOfTheDay().then((image) => {
-//     let iotd = './temp/bingiotd.jpg'
-//     fs.writeFileSync(iotd, image.imageData, {encoding: 'binary'})
-
-//     mosaic.generate(fs.readFileSync(iotd), output, colorTable, {
-//         emojiSize: emojiSize
-//     })
-//     .then(() => {
-//         let text = `Bing image of the day #emojified (source:${image.imageUrl})`
-//         let altText = 'Bing image of the day, emojified'
-//         bot.tweetReply(text, 0, output, altText)
-//     })
-//     .catch((reason) => {console.log(reason)})
 // })
--- mosaic.js
+++ mosaic.js
@@ -22,7 +22,7 @@ function resize(file, outFile, width, height, format) {
 }
 
 function reduceImageToColor(imageFile) {
-    let rgbaFile = path.join(os.tmpdir(), '1x1.png')
+    let rgbaFile = path.join(os.tmpdir(), `1x1_${path.basename(imageFile)}.rgba`)
     return resize(imageFile, rgbaFile, 1, 1, 'RGBA').then((path) => {
         let bytes = fs.readFileSync(rgbaFile)
         let rgb = {
@@ -42,86 +42,120 @@ function reduceImageToColor(imageFile) {
     })
 }
 
-function createColorTable(folder, cacheBase, emojiSize) {
-    if (!fs.existsSync(cacheBase)) {
-        fs.mkdirSync(cacheBase)
+class Tileset {
+    constructor(folder, cacheBase, emojiSize) {
+        this.folder = folder
+        this.cacheBase = cacheBase
+        this.emojiSize = emojiSize
+        this.hslTable = null
     }
-    return new Promise((resolve, reject) => {
+
+    colorTableRecurse(i, files, colors) {
+        let source = path.join(this.folder, files[i])
+        let resized = path.join(this.cacheBase, files[i])
+
+        //console.log(`File ${source}`)
+        return resize(source, resized, this.emojiSize, this.emojiSize, 'PNG24').then((thumb) => {
+            //console.log(`Reduce image to color ${resized}`)
+            return reduceImageToColor(resized)
+        }).then((color) => {
+            //console.log(`${resized} = (${color.red}, ${color.green}, ${color.blue})`)
+            colors[resized] = color
+            if (i < files.length - 1) {
+                return this.colorTableRecurse(i+1, files, colors)
+            }
+        })
+    }
+
+    generateColorTableAndCache(colorTableFile) {
         var operations = []
         var colors = {}
-        var files = fs.readdirSync(folder)
-        for (var file of files) {
-            var source = path.join(folder, file)
-            var resized = path.join(cacheBase, file)
+        let files = fs.readdirSync(this.folder)
+        return this.colorTableRecurse(0, files, colors).then(() => {
+            fs.writeFileSync(colorTableFile, JSON.stringify(colors))
+        })
+    }
 
-            operations.push(resize(source, resized, emojiSize, emojiSize, 'PNG24'))
-            operations.push(reduceImageToColor(source).then((color) => {
-                colors[resized] = color
-            }))
+    load() {
+        if (this.hslTable != null) {
+            return new Promise((resolve, reject) => resolve())
         }
-        Promise.all(operations).then((res) => resolve(colors))
-    })
-}
 
-exports.createColorTable = createColorTable
+        if (!fs.existsSync(this.cacheBase)) {
+            fs.mkdirSync(this.cacheBase)
+        }
 
-function colorTableToHslTable(colorTable) {
-    var hslTable = {}
-    for (var file in colorTable) {
-        var c = colorTable[file]
-        var hsl = rgbToHsl(c.red, c.green, c.blue) // [hue, "sat%", "lum%"]
+        let colorTableFile = path.join(this.cacheBase, 'colors.json')
+        return new Promise((resolve, reject) => {
+            if (!fs.existsSync(colorTableFile)) {
+                this.generateColorTableAndCache(colorTableFile).then((colorTable) => {
+                    resolve(colorTable)
+                })
+            }
+            else {
+                resolve(JSON.parse(fs.readFileSync(colorTableFile)))
+            }
+        })
+        .then((colorTable) => {
+            this.hslTable = {}
+            for (var file in colorTable) {
+                var c = colorTable[file]
+                var hsl = rgbToHsl(c.red, c.green, c.blue) // [hue, "sat%", "lum%"]
+                var hue = Math.floor(hsl[0])
+                if (this.hslTable[hue] === undefined) {
+                    this.hslTable[hue] = []
+                }
+                this.hslTable[hue] = [...this.hslTable[hue], {
+                    hue: hue, 
+                    sat: parseFloat(hsl[1].substring(0, hsl[1].length-1)) / 100,
+                    lum: parseFloat(hsl[2].substring(0, hsl[2].length-1)) / 100,
+                    file: file
+                }]
+            }
+        })
+    }
+
+    findTileForPixel(rgb, threshold) {
+        var hsl = rgbToHsl(rgb.red, rgb.green, rgb.blue)
         var hue = Math.floor(hsl[0])
-        if (hslTable[hue] === undefined) {
-            hslTable[hue] = []
+        var sat = parseFloat(hsl[1].substring(0, hsl[1].length-1)) / 100
+        var lum = parseFloat(hsl[2].substring(0, hsl[2].length-1)) / 100
+        
+        for (var i=0; i < threshold; i++) {
+            if ((hue + i) in this.hslTable) {
+                var best = this.hslTable[(hue + i)][0]
+                var delta = Math.sqrt(Math.pow(best.sat - sat, 2) + Math.pow(best.lum - lum, 2))
+                for (var opt of this.hslTable[(hue + i)].slice(1)) {
+                    var d = Math.sqrt(Math.pow(opt.sat - sat, 2) + Math.pow(opt.lum - lum, 2))
+                    if (d < delta) {
+                        best = opt
+                        delta = d
+                    }
+                }
+                if (delta < (i * 0.05)) {
+                    return best.file
+                }
+            }
+            if ((hue - i) in this.hslTable) {
+                var best = this.hslTable[(hue - i)][0]
+                var delta = Math.sqrt(Math.pow(best.sat - sat, 2) + Math.pow(best.lum - lum, 2))
+                for (var opt of this.hslTable[(hue - i)].slice(1)) {
+                    var d = Math.sqrt(Math.pow(opt.sat - sat, 2) + Math.pow(opt.lum - lum, 2))
+                    if (d < delta) {
+                        best = opt
+                        delta = d
+                    }
+                }
+                if (delta < (i * 0.05)) {
+                    return best.file
+                }
+            }
         }
-        hslTable[hue] = [...hslTable[hue], {
-            hue: hue, 
-            sat: parseFloat(hsl[1].substring(0, hsl[1].length-1)) / 100,
-            lum: parseFloat(hsl[2].substring(0, hsl[2].length-1)) / 100,
-            file: file
-        }]
+        throw new Error('Could not find color match for RGB ' + JSON.stringify(rgb) + ' ' + JSON.stringify(hsl))
     }
-    return hslTable
 }
 
-function findTileForPixel(rgb, hslTable) {
-    var hsl = rgbToHsl(rgb.red, rgb.green, rgb.blue)
-    var hue = Math.floor(hsl[0])
-    var sat = parseFloat(hsl[1].substring(0, hsl[1].length-1)) / 100
-    var lum = parseFloat(hsl[2].substring(0, hsl[2].length-1)) / 100
-    
-    for (var i=0; i < 30; i++) {
-        if ((hue + i) in hslTable) {
-            var best = hslTable[(hue + i)][0]
-            var delta = Math.sqrt(Math.pow(best.sat - sat, 2) + Math.pow(best.lum - lum, 2))
-            for (var opt of hslTable[(hue + i)].slice(1)) {
-                var d = Math.sqrt(Math.pow(opt.sat - sat, 2) + Math.pow(opt.lum - lum, 2))
-                if (d < delta) {
-                    best = opt
-                    delta = d
-                }
-            }
-            if (delta < (i * 0.05)) {
-                return best.file
-            }
-        }
-        if ((hue - i) in hslTable) {
-            var best = hslTable[(hue - i)][0]
-            var delta = Math.sqrt(Math.pow(best.sat - sat, 2) + Math.pow(best.lum - lum, 2))
-            for (var opt of hslTable[(hue - i)].slice(1)) {
-                var d = Math.sqrt(Math.pow(opt.sat - sat, 2) + Math.pow(opt.lum - lum, 2))
-                if (d < delta) {
-                    best = opt
-                    delta = d
-                }
-            }
-            if (delta < (i * 0.05)) {
-                return best.file
-            }
-        }
-    }
-    throw new Error('Could not find color match for RGB ' + JSON.stringify(rgb))
-}
+exports.Tileset = Tileset
 
 function renderTile(columns, rows, start, rowOffset, emojis, emojiSize, dest, callback) {
     var cmd = []
@@ -137,18 +171,20 @@ function renderTile(columns, rows, start, rowOffset, emojis, emojiSize, dest, ca
     im.convert(cmd, callback)
 }
 
-function generate(source, dest, colorTable, options, callback) {
+function generate(source, dest, tileset, options, callback) {
     var verbose = (options !== undefined && "verbose" in options ? options.verbose : false)
     var emojiSize = (options !== undefined && "size" in options ? options.size : 16)
     var maxMosaicSize = (options !== undefined && "maxMosaicSize" in options ? options.maxMosaicSize : 200)
+    var threshold = (options !== undefined && "threshold" in options ? options.threshold : 30)
     
-    var hslTable = colorTableToHslTable(colorTable)
-
     var columns = 0, rows = 0
-    return new Promise((resolve, reject) => {
-        im.identify(source, (err, identity) => {
-            if (err) throw err
-            resolve(identity)
+    return tileset.load()
+    .then(() => {
+        return new Promise((resolve, reject) => {
+            im.identify(source, (err, identity) => {
+                if (err) throw err
+                resolve(identity)
+            })
         })
     })
     .then((identity) => {
@@ -184,7 +220,7 @@ function generate(source, dest, colorTable, options, callback) {
                 blue: normalize(bytes[p*4+2], bytes[p*4+3]),
             }
             if (verbose) console.log("Target pixel rgb: " + JSON.stringify(rgb))
-            emojis.push(findTileForPixel(rgb, hslTable))
+            emojis.push(tileset.findTileForPixel(rgb, threshold))
         }
         if (emojis.length != rows * columns) {
             throw new Error(`Bad emoji-map size, expected: ${rows * columns}, actual: ${emojis.length}`)
