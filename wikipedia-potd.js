650eee5a6ec6952c51c3ba6c9a8fcc245eb5eeb7
Brian Holley <brian.holley@hotmail.com>
2017-02-26 15:50:18 -0800

    Use higher resolution images from Wikipedia

--- wikipedia-potd.js
+++ wikipedia-potd.js
@@ -11,10 +11,15 @@ function imageOfTheDay() {
     let url = `https://en.wikipedia.org/wiki/Template:POTD/${getYYYYMMDD()}`
     return rp({uri: url}).then(html => {
         let $ = cheerio.load(html)
-        let img = $("div#content img")
+        let a = $("div#content a.image")
+        let imageUrl = "https://en.wikipedia.org" + a.attr("href")
+        return rp({uri: imageUrl})
+    }).then(html => {
+        let $ = cheerio.load(html)
+        let img = $("div#file img")
         let srcset = img.attr("srcset").split(" ")
         return {
-            imageUrl: "http:" + srcset[srcset.length - 2],
+            imageUrl: "https:" + srcset[srcset.length-2],
             altText: img.attr("alt")
         }
     })
